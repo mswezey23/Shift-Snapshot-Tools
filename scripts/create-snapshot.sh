0ada5c5960ee15f8d851c2acee51937ecccc0ceb
Matt Swezey <matt@swezey.io>
2020-03-03 12:19:18 -0600

    update script

--- scripts/create-snapshot.sh
+++ scripts/create-snapshot.sh
@@ -2,11 +2,12 @@
 
 # Snapshot creation, assumes "shift_db" as the DB name
 DB_SNAPSHOT="blockchain.db.gz"
-DB_PATH="/home/$USER/snapshots/"
+DB_PATH="/home/$USER/snapshots"
 DB_NAME="shift_db"
 
-res=$(mkdir -p "home/$USER/snapshots/")
+res=$(mkdir -p $DB_PATH)
+res=$(touch $DB_PATH/$DB_SNAPSHOT)
 echo -n  "Dumping DB to filesytem..."
-res=$(sudo -u postgres pg_dump  "$DB_NAME" | gzip -9 > "/home/$USER/snapshots/$DB_SNAPSHOT")
-# res=$(sudo -u shiftcore mv "/home/$USER/$DB_SNAPSHOT" "$DB_PATH$DB_SNAPSHOT")
+res=$(sudo -u postgres pg_dump  "$DB_NAME" | gzip -9 > "$DB_PATH/$DB_SNAPSHOT")
+# res=$(sudo -u shiftcore mv "/home/$USER/$DB_SNAPSHOT" "$DB_PATH/$DB_SNAPSHOT")
 echo -n "Complete!"
